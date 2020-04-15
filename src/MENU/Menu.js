a1116bf396ea14e154c9279bebd4c6d8e62207b3
Shubham <57shubhamkrsingh@gmail.com>
2020-04-15 18:44:38 +0530

    Changed Menu

--- src/MENU/Menu.js
+++ src/MENU/Menu.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import Slideshow from './Slideshow';
 import Carddetails from './Carddetails';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faHome, faPhoneSquareAlt, faVideo, faSearch, faUser, faVoicemail} from '@fortawesome/free-solid-svg-icons'
+import {faPhoneAlt, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn, faTwitterSquare, faFacebook, faGooglePlus, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import InputBase from '@material-ui/core/InputBase';
 // import Button from '@material-ui/core/Button';
 import Sidemenu from './Sidemenu';
@@ -17,25 +18,41 @@ function Menu() {
     return (
         <div>
             <nav className='logo'>
-                
+            <span><FontAwesomeIcon icon={faPhoneAlt}/>  +91-7979070712 & +91-8092290510</span>
+            <span><FontAwesomeIcon icon={faEnvelope}/>  57shubhamkrsingh@gmail.com</span>
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <span>
+                <FontAwesomeIcon icon={faFacebook} title='Facebook'/>
+                &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+                <FontAwesomeIcon icon={faGooglePlus} title='Google Plus'/>
+                &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+                <FontAwesomeIcon icon={faTwitterSquare} title='Twitter'/>
+                &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+                <FontAwesomeIcon icon={faLinkedinIn} title='LinkedIn'/>
+                &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+                <FontAwesomeIcon icon={faYoutube} title='Youtube'/>
+                </span>         
             </nav>
         <nav className='Menu'>    
         <Sidemenu/>     
         <ul>               
         {/* <FontAwesomeIcon icon={faSearch} InputBase placeholder='search…' className='buttn'  /> */}
-        <li><Link to = "/Home"><button className='buttn' title='Home'> HOME </button> </Link></li>
-        <li> <Link to = "/About"> <button className='buttn' title='Videos'> ABOUT</button> </Link>  </li>  
+        <li><Link to = "/Home"><button className='buttn' title='Home'> Home </button> </Link></li>
+        <li> <Link to = "/About"> <button className='buttn' title='Videos'> About</button> </Link>  </li>  
         <li><Link to = "/Contact"> <button className='buttn' title='Contact Us'> Templates</button></Link> </li>
         <li><Link to = "/Contact"> <button className='buttn' title='Contact Us'> Blog</button></Link> </li>
         <li><Link to = "/Contact"> <button className='buttn' title='Contact Us'> Gallery</button></Link> </li>
         <li><Link to = "/Contact"> <button className='buttn' title='Contact Us'> Contact Us</button></Link> </li>
         </ul>
-        <ul>
-                    <li><FontAwesomeIcon icon={faVoicemail}/></li>
-                    <li><FontAwesomeIcon icon={faUser}/></li>
-                    <li><FontAwesomeIcon icon={faUser}/></li>
-                    <li><FontAwesomeIcon icon={faUser}/></li>
-                </ul>
+        
+            
         </nav>
         <h1 className='h1'>“Live with no excuses and travel with no regrets”</h1>
         <Sidemenuitems/>
